8c31f22fce2c3ca2f214de702870225782ac096c
Joshua Rosenberg <jrosen@msu.edu>
2018-11-30 23:36:56 -0500

    update

--- download.py
+++ download.py
@@ -2,6 +2,6 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"Math memes","limit":200,"print_urls":True}   #creating list of arguments
+arguments = {"keywords":"mathematics memes, math memes","limit":99,"print_urls":True}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
